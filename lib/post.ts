1ce5772054b90810a6c21a73e54b1909b564f21d
joo <jinjooisme@gmail.com>
2022-09-15 11:41:18 +0900

    feat: show post data by using remark

--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
 
 // /posts 경로
 const postsDirectory = path.join(process.cwd(), "posts");
@@ -34,4 +36,35 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  const matterResult = matter(fileContents);
+
+  // use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,50 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import React from "react";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import homeStyles from "../../styles/Home.module.css";
 
-export default function Post() {
-  return <div>Post</div>;
-}
+const Post = ({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) => {
+  return (
+    <div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={homeStyles.headingXl}>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  console.log("paths", paths);
+  // [{ params: { id: 'pre-rendering' }, ... }]
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  console.log("params", params);
+  const postData = await getPostData(params.id as string);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
